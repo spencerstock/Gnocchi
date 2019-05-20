8423376685e48971ef7190dc87fe88d5a0e8d989
Spencer Stock <spencerstock@gmail.com>
2019-05-20 16:55:51 -0600

    added basic functionality for main activity

--- app/src/main/java/com/spencerstock/gnocchi/MainActivity.java
+++ app/src/main/java/com/spencerstock/gnocchi/MainActivity.java
@@ -1,13 +1,52 @@
 package com.spencerstock.gnocchi;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CODE = 123;
+    Button buttonNewGnocchi;
+    Button buttonAddPhoto;
+    ImageButton button_test_gnocchi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonAddPhoto = findViewById(R.id.button_add_photo);
+        buttonNewGnocchi = findViewById(R.id.button_new);
+        button_test_gnocchi = findViewById(R.id.test_imageButton);
+
+
+        buttonAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CODE);
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            button_test_gnocchi.setImageBitmap(imageBitmap);
+        }
     }
 }
